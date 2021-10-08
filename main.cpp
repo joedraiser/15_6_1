c604fedda5924b79f59e6e10d19c7d25f29f7c75
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-08 17:40:43 +0300

    Removed 2nd loop

--- main.cpp
+++ main.cpp
@@ -3,25 +3,40 @@
 
 int main()
 {
-    std::vector<int> a = {-2,1,-3,4,-1,2,1,-5,4};
-    int max=a[0];
-    int sum=0;
-    int upperBnd, lowerBnd;
+    std::vector<int> a = {-2, -3, 4, -1, 2, 1, -5, 3};
+    int max=INT_MIN;
+    int sum=0, wasSumPositiveBefore=0;
+    int lowerBnd, upperBnd, currentLowerBnd=0, currentUpperBnd=0;
 
-    for(int i=0;i<a.size()-1;i++)
+    for(int i=0;i<a.size();i++)
     {
-        for(int j=i;j<a.size();j++)
+        sum+=a[i];
+        if(sum > 0 && !wasSumPositiveBefore)
         {
-            sum += a[j];
-            if(sum>max)
-            {
-                max = sum;
-                upperBnd=i;
-                lowerBnd=j;
-            }
+            wasSumPositiveBefore=1;
+            currentLowerBnd=i;
+            currentUpperBnd=i;
         }
-        sum=0;
+        else if(sum > 0 && wasSumPositiveBefore)
+        {
+            currentUpperBnd=i;
+        }
+
+        if(sum>max)
+        {
+            max=sum;
+            lowerBnd=currentLowerBnd;
+            upperBnd=currentUpperBnd;
+        }
+
+        if(sum < 0)
+        {
+            wasSumPositiveBefore=0;
+            currentLowerBnd=i;
+            currentUpperBnd=i;
+            sum=0;
+        }
     }
 
-    std::cout << upperBnd << " " << lowerBnd;
+    std::cout << lowerBnd << " " << upperBnd;
 }
